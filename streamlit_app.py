e106bde6fb9e5eb3f7de51b8d61ce624dda26117
hasifah1717-ctrl <hasifah1717@gmail.com>
2026-05-19 11:29:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,7 @@ number = st.number_input(
     "Insert a number", value=None, placeholder="Type a number..."
 )
 st.write("The current number is ", number)
+import streamlit as st
+
+number = st.number_input("Insert a number")
+st.write("The current number is ", number)
